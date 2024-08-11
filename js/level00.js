a0ac3dce380ec387ce8f5c4c68a18ed590f8888d
Kasikiroti <kasikiroti@gmail.com>
2024-08-10 19:25:50 -0600

    intento de balazos :v

--- js/level00.js
+++ js/level00.js
@@ -28,12 +28,16 @@ class gameArea{
     if(this.keyPressed){
         if(this.key == "ArrowDown" || this.key == "KeyS"){
             this.player.moveY(1)
+            this.player.direction = "down";
         } else if(this.key == "ArrowUp" || this.key == "KeyW"){
             this.player.moveY(-1);
+            this.player.direction = "up";
         } else if(this.key == "ArrowRight" || this.key == "KeyD"){
             this.player.moveX(1);
+            this.player.direction = "right";
         } else if(this.key == "ArrowLeft" || this.key == "KeyA"){
             this.player.moveX(-1);
+            this.player.direction = "left";
         } else if(this.key == "Space"){
             this.player.fire(this.player.x, this.player.y)
         }
@@ -80,6 +84,7 @@ class player extends sprite{
     constructor(x, y, w, h, speed, color) {
         super(x, y, w, h, speed, color);
         this.shooting = false
+        this.direction = "right";
     };
     
     draw(){
@@ -100,22 +105,52 @@ class player extends sprite{
     }
    async fire(x, y){
     if(!this.shooting){
-        const shoot = new sprite(x+50, y+12, 50, 25, 20, "yellow")
+ //ayuda jefesita :,,v       const shoot = new sprite(x+50, y+12, 50, 25, 20, "yellow")
+        let shoot;
+        const direction = this.direction;
+        switch (direction){
+            case "up":
+                shoot = new sprite(x-12, y-50, 25, 50, 20, "yellow");
+            break;
+            case "down":
+                shoot = new sprite(x-12, y-50, 25, 50, 20, "yellow");
+            break;
+            case "left":
+                shoot = new sprite(x-50, y+12, 50, 25, 20, "yellow");
+            break;
+            case "right":
+                shoot = new sprite(x+50, y+12, 50, 25, 20, "yellow");
+            break;
+        }
         this.shooting = true
         shoot.draw();
         let reachBarrier = false
-        while (shoot.x < canvas.width-60) {
+        while (shoot.x < canvas.width-60 && shoot.y < canvas.height - 60 && shoot.y > 0 && shoot.x > 0) {
             if( (shoot.x > canvas.width * 0.5) && !reachBarrier){
                 this.shooting = false
                 barrier.draw()
                 reachBarrier = true
             }
             shoot.clear()
-            shoot.moveX(1)
+            switch (direction){
+                case "right":
+                    shoot.moveX(1);
+                break;
+                case "left":
+                    shoot.moveX(-1);
+                break;
+                case "up":
+                    shoot.moveY(-1);
+                break;
+                case "down":
+                    shoot.moveY(1);
+                break;
+            }
             shoot.draw()
             await new Promise(resolve => setTimeout(resolve, 100))
         }
         shoot.clear();
+        this.shooting = false
     }
 }
 }
