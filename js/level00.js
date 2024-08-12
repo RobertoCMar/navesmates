4b1f11950863b15a33113a561c4456f08c46ddb2
RobertoC <[robertomtz_10@hotmail.com]>
2024-08-12 14:50:49 -0600

    cambio en las carpetas :V

--- js/level00.js
+++ js/level00.js
@@ -247,10 +247,10 @@ class player extends sprite{
 }
 }
 const boo = document.getElementById("boo")
-const enemy = new player({image: boo, x: 400, y: 400, width: 50, height: 50, speed: 10, color: "red"})
+const enemy = new player({image: boo, x: 400, y: 400, width: 50, height: 50, speed: 10})
 enemy.draw()
 // Nmms se ve bien cagado XDDDDD
-const game = new gameArea({image: boo, x: 0, y: 0, width: 50, height: 50, speed: 10, color: "blue"});
+const game = new gameArea({image: boo, x: 0, y: 0, width: 50, height: 50, speed: 10});
 game.player.draw()
 game.update()
 
