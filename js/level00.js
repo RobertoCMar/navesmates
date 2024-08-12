594e688bb512ad635f045e2381150efbdcd80fe9
RobertoC <[robertomtz_10@hotmail.com]>
2024-08-11 18:44:52 -0600

    daño y bla bla

--- js/level00.js
+++ js/level00.js
@@ -5,10 +5,10 @@ canvas.height = window.innerHeight - window.innerHeight*0.1
 const ctx = canvas.getContext("2d");
 
 class gameArea{
-   constructor({x = 0, y = 0, w, h, speed = 10, color}){
+   constructor({x = 0, y = 0, width, height, speed, color}){
         this.keyPressed = false,
         this.key = null,
-        this.player = new player(x, y, w, h, speed, color),
+        this.player = new player({x, y, width, height, speed, color}),
         this.control()
    }
 
@@ -53,11 +53,11 @@ class gameArea{
 }
 
 class sprite{
-    constructor(x, y, w, h, speed, color){
+    constructor({x, y, width, height, speed = 0, color}){
         this.x = x,
         this.y = y,
-        this.w = w,
-        this.h = h,
+        this.width = width,
+        this.height = height,
         this.speed = speed,
         this.color = color
     }
@@ -71,79 +71,135 @@ class sprite{
     }
 
     clear(){
-        ctx.clearRect(this.x, this.y, this.w, this.h)
+        ctx.clearRect(this.x, this.y, this.width, this.height)
     }
 
     draw(){
         ctx.fillStyle = this.color
-        ctx.fillRect(this.x, this.y, this.w, this.h)
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+
+    isInContact(objX, objY, objWidth, objHeight){
+        return (
+            this.x + this.width >= objX && 
+            this.x <= objX + objWidth && 
+            this.y + this.height >= objY && 
+            this.y <= objY + objHeight 
+        )
     }
 }
 
 class player extends sprite{
-    constructor(x, y, w, h, speed, color, health) {
-        super(x, y, w, h, speed, color);
+    constructor({
+            x, y, width, height, color, 
+            speed = 10, health = 100, 
+            damage = 10, atkWidth = 50, atkHeight = 25, atkSpeed = 25, atkColor = "yellow"
+        }) {
+        super({x, y, width, height, speed, color});
         this.shooting = false
         this.direction = "right";
         this.health = health;
+        this.lives = true
+        this.damage = damage
+        this.atkWidth = atkWidth
+        this.atkHeight = atkHeight
+        this.atkSpeed = atkSpeed
+        this.atkColor = atkColor
     };
     
     draw(){
         ctx.fillStyle = this.color
-        ctx.fillRect(this.x, this.y, this.w, this.h)
+        ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 
-    moveX(x) {
-        if (this.x + this.speed * x >= 0 && this.x + this.speed * x + this.w <= canvas.width * 0.5) {
-            this.x += this.speed * x;
+    moveX(x) { 
+        if (this.x + this.speed * x >= 0 && this.x + this.speed * x + this.width <= canvas.width) {
+                this.x += this.speed * x;
+                if(this.isInContact(enemy.x, enemy.y, enemy.width, enemy.height)){
+                    this.x -= this.speed * x
+                }
         }
     }
 
     moveY(y) {
-        if (this.y + this.speed * y >= 0 && this.y + this.speed * y + this.h <= canvas.height) {
-            this.y += this.speed * y;
+        if (this.y + this.speed * y >= 0 && this.y + this.speed * y + this.height <= canvas.height) {
+                this.y += this.speed * y;
+            if(this.isInContact(enemy.x, enemy.y, enemy.width, enemy.height)){
+                this.y -= this.speed * y;
+            }
         }
     }
 
-    addhealth(n){
+    addHealth(n){
         this.health += n;
     }
 
-    damage(n){
+    minusHealth(n){
         this.health -= n
+        if(this.health <= 0){
+            this.lives = false
+            this.clear()
+        } else {
+            console.log(this.health)
+            this.draw()
+        }
     }
 
    async fire(x, y){
     if(!this.shooting){
  //ayuda jefesita :,,v       const shoot = new sprite(x+50, y+12, 50, 25, 20, "yellow")
         let shoot;
-        const bulletWidth = 50
-        const bulletHeight = 25
-        const direction = this.direction;
-        const bulletSpeed = 20
-        const bulletColor = "yellow"
+        const direction = this.direction
         switch (direction){
             case "up":
-                shoot = new sprite(x+Math.floor(bulletHeight*0.5), y-bulletWidth, bulletHeight, bulletWidth, bulletSpeed, bulletColor);
+                shoot = new sprite({
+                    x: x+Math.floor(this.atkHeight*0.5), 
+                    y: y-this.atkWidth, 
+                    width: this.atkHeight, 
+                    height: this.atkWidth, 
+                    speed: this.atkSpeed, 
+                    color: this.atkColor
+                });
             break;
             case "down":
-                shoot = new sprite(x+Math.floor(bulletHeight*0.5), y+bulletWidth, bulletHeight, bulletWidth, bulletSpeed, bulletColor);
+                shoot = new sprite({
+                    x: x+Math.floor(this.atkHeight*0.5), 
+                    y: y+this.atkWidth, 
+                    width: this.atkHeight, 
+                    height: this.atkWidth, 
+                    speed: this.atkSpeed, 
+                    color: this.atkColor
+                });
             break;
             case "left":
-                shoot = new sprite(x-bulletWidth, y+Math.floor(bulletHeight*0.5), bulletWidth, bulletHeight, bulletSpeed, bulletColor);
+                shoot = new sprite({
+                    x: x-this.atkWidth, 
+                    y: y+Math.floor(this.atkHeight*0.5), 
+                    width: this.atkWidth, 
+                    height: this.atkHeight, 
+                    speed: this.atkSpeed, 
+                    color: this.atkColor
+                });
             break;
             case "right":
-                shoot = new sprite(x+bulletWidth, y+Math.floor(bulletHeight*0.5), bulletWidth, bulletHeight, bulletSpeed, bulletColor);
+                shoot = new sprite({
+                   x: x+this.atkWidth, 
+                   y: y+Math.floor(this.atkHeight*0.5), 
+                   width: this.atkWidth, 
+                   height: this.atkHeight, 
+                   speed: this.atkSpeed, 
+                   color: this.atkColor
+                });
             break;
         }
         this.shooting = true
         shoot.draw();
-        let reachBarrier = false
         while (shoot.x < canvas.width-60 && shoot.y < canvas.height - 60 && shoot.y > 0 && shoot.x > 0) {
-            if( (shoot.x > canvas.width * 0.5) && !reachBarrier){
+            if(shoot.isInContact(enemy.x, enemy.y, enemy.width, enemy.height)){
+                shoot.clear()
                 this.shooting = false
-                barrier.draw()
-                reachBarrier = true
+                enemy.minusHealth(this.damage)
+                return
             }
             shoot.clear()
             switch (direction){
@@ -168,9 +224,9 @@ class player extends sprite{
     }
 }
 }
-const barrier = new sprite(canvas.width*0.5, 0, 5, canvas.height, 0, "red")
-barrier.draw()
-const game = new gameArea({w: 50, h: 50, color: "blue", health: 100});
+const enemy = new player({x: 400, y: 400, width: 50, height: 50, speed: 10, color: "red"})
+enemy.draw()
+const game = new gameArea({x: 0, y: 0, width: 50, height: 50, speed: 10, color: "blue"});
 game.player.draw()
 game.update()
 
