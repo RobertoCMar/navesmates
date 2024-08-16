901301e42314150f7ad6b5481b517bd6e10e5260
Kasikiroti <kasikiroti@gmail.com>
2024-08-15 18:33:07 -0600

    No more arboles

--- js/config.js
+++ js/config.js
@@ -5,41 +5,6 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight - window.innerHeight*0.1
 const ctx = canvas.getContext("2d");
 ctx.font = "15px Verdana";
-const treeImg = new Image();
-treeImg.src = "./../Assets/tree.png";
-
-treeImg.onload = () => {
-    drawTrees();
-};
-
-function drawTrees() {
-    const treeSize = 30; // Tamaño de cada árbol
-    const treePositions = [];
-
-    // Árboles en la parte superior
-    for (let x = 0; x < canvas.width; x += treeSize) {
-        treePositions.push({ x: x, y: 0 });
-    }
-
-    // Árboles en la parte inferior
-    for (let x = 0; x < canvas.width; x += treeSize) {
-        treePositions.push({ x: x, y: canvas.height - treeSize });
-    }
-
-    // Árboles en el lado izquierdo
-    for (let y = 0; y < canvas.height; y += treeSize) {
-        treePositions.push({ x: 0, y: y });
-    }
-
-    // Árboles en el lado derecho
-    for (let y = 0; y < canvas.height; y += treeSize) {
-        treePositions.push({ x: canvas.width - treeSize, y: y });
-    }
-
-    treePositions.forEach(pos => {
-        ctx.drawImage(treeImg, pos.x, pos.y, treeSize, treeSize);
-    });
-}
 
 class gameArea{
    constructor({x = Math.floor(canvas.width * 0.5), y = Math.floor(canvas.height * 0.5), width, height, image, 
